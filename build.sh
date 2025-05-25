d8ea08efd5e276fcf2c3a630b09e7f5750a7d35e
Suraj9411 <surajrwt9411@gmail.com>
2025-05-25 16:46:33 +0530

    update

---
+++ build.sh
@@ -0,0 +1,9 @@
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files & run migrations
+python manage.py collectstatic --noinput
+python manage.py migrate
