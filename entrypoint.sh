0ee8cb382272ae4c052083bf4a9d7c09bdef82c2
Suraj Singh Rawat <91652123+Suraj9411@users.noreply.github.com>
2025-05-26 16:56:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Waiting for database to be ready..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
+python3 manage.py createhorillauser --first_name suraj --last_name rawat --username suraj --password admin123 --email surajrwtbro9411@gmail.com --phone 8433260352
 
 # Start Gunicorn on the port Render provides
 exec gunicorn horilla.wsgi:application --bind 0.0.0.0:$PORT
